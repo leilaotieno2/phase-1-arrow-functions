e5168eed5a49c1ed2287af90645b866f9db875b1
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-04-03 11:57:33 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+const divide = function (Number1,Number2) {
+  Number1 =2000
+  Number2 = 100
+  const result = parseFloat(Number1) / parseFloat(Number2);
+  return result;
+}
+
+
+
+const square = (number) => {
+  return number * number;
+}
+
+const add =(n1,n2)=>
+{
+return n1 + n2
+}
+
